a61d285ab4447df55e732c4695495c9b23b896dd
nsfern <natfernsten@gmail.com>
2019-11-19 16:26:19 +0000

    my first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Natalie Fernsten Period 1]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Fernsten_AboutMe.html">AboutMe.html</a></li></br>
+       <li><a href="Fernsten_Resume.html">Resume.html</a></li></br>
+      <li><a href="Fernsten_Projects.html">Projects.html</a></li></br>
+      <li><a href="Fernsten_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
